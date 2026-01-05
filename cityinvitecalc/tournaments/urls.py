6d717f853aa3b12d4b48f25f975cffcbc5dab568
Ryan Bown <rbown1993@hotmail.com>
2026-01-05 12:38:06 -0500

    added player edit forms

--- cityinvitecalc/tournaments/urls.py
+++ cityinvitecalc/tournaments/urls.py
@@ -3,9 +3,12 @@ from . import views
 
 app="tournaments"
 urlpatterns = [
+    path('', views.HomeView, name='home'),
     path('view/', views.TournamentView, name='tournaments-view'),
     path('view/<str:tournament_id>', views.TournamentDetailView, name='tournaments-detail'),
-    path('player/<str:player_id>', views.PlayerView, name='tournaments-detail'),
+    path('player/<str:player_id>', views.PlayerView, name='player'),
+    path('edit_player/<str:player_id>', views.EditPlayerView, name='edit-player'),
+    path('players', views.ListPlayersView, name='all-players'),
     path('view/tdf/', views.TdfView, name='all-tdf'),
     path('view/tdf/<int:file_id>', views.ModifyTdfView, name='modify-tdf'),
 ]
