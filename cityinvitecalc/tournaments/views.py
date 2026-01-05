7285eda0465fb01517543177624a569bfcf69bc8
Ryan Bown <rbown1993@hotmail.com>
2026-01-05 12:35:09 -0500

    create home page, list and edit page

--- cityinvitecalc/tournaments/views.py
+++ cityinvitecalc/tournaments/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Tournament, TournamentDetail, Player, FileRun
+from .forms import PlayerForm
 
-
+from django.http import HttpResponseRedirect
 # Create your city here.
 
-
+def HomeView(request):
+    return render(request, 'Home.html')
 
 def TournamentView(request):
     all_tournaments = Tournament.objects.all()
@@ -16,10 +19,32 @@ def TournamentDetailView(request, tournament_id):
     print(this_tournament)
     return render(request, 'TournamentDetail.html', context= {'this_tournament':this_tournament})
 
+def ListPlayersView(request, msg = []):
+
+    players = Player.objects.all().order_by('-division__sort_order', 'last_name', 'first_name')
+
+    return render(request, 'ListPlayers.html', context={'players':players, 'messages':msg})
 
 def PlayerView(request, player_id):
-    player = Player.objects.filter(pokemon_id = player_id)
+
+    player = Player.objects.get(pokemon_id = player_id)
     return render(request, 'Player.html', context={'player':player})
+
+def EditPlayerView(request, player_id):
+    if Player.objects.filter(pokemon_id = player_id).exists():
+        player = Player.objects.get(pokemon_id = player_id)
+    else:
+        return HttpResponseRedirect("Player Does Not Exist")
+    if request.method == "POST":
+        form = PlayerForm(request.POST, instance=player)
+        if form.is_valid():
+            form.save()
+            msg = messages.add_message(request, messages.INFO, "Save Successful")
+            return ListPlayersView(request, msg )
+    else:
+        form = PlayerForm(instance=player)
+
+    return render(request, "EditPlayer.html", {"form": form})
 
 
 def TdfView(request):
