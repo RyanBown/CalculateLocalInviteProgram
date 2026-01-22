5190c2cb709c8d2e87a1c11306a417e391a04609
Ryan Bown <rbown1993@hotmail.com>
2026-01-22 13:37:20 -0500

    added edit player in tournament

--- cityinvitecalc/tournaments/urls.py
+++ cityinvitecalc/tournaments/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('view_tournament/', views.TournamentView, name='tournaments-view'),
     path('view_tournament/<str:tournament_id>', views.TournamentDetailView, name='tournaments-detail'),
     path('view_player/', views.ListPlayersView),
+    path('edit_player_in_tournament/<str:tournament_id>/<str:player_id>', views.edit_player_in_tournament, name='edit-player-in-tournament'),
     path('view_player/<str:player_id>', views.PlayerView, name='player'),
     path('edit_player/<str:player_id>', views.EditPlayerView, name='edit-player'),
     path('players', views.ListPlayersView, name='all-players'),
