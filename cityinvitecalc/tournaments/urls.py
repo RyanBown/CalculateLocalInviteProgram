da1a2ad7c0963bfdbe5914dbc4a9881bfcc3a0b2
Ryan Bown <rbown1993@hotmail.com>
2026-01-22 12:30:09 -0500

    added view all players and modified URLs to be less confusing

--- cityinvitecalc/tournaments/urls.py
+++ cityinvitecalc/tournaments/urls.py
@@ -4,9 +4,10 @@ from . import views
 app="tournaments"
 urlpatterns = [
     path('', views.HomeView, name='home'),
-    path('view/', views.TournamentView, name='tournaments-view'),
-    path('view/<str:tournament_id>', views.TournamentDetailView, name='tournaments-detail'),
-    path('player/<str:player_id>', views.PlayerView, name='player'),
+    path('view_tournament/', views.TournamentView, name='tournaments-view'),
+    path('view_tournament/<str:tournament_id>', views.TournamentDetailView, name='tournaments-detail'),
+    path('view_player/', views.ListPlayersView),
+    path('view_player/<str:player_id>', views.PlayerView, name='player'),
     path('edit_player/<str:player_id>', views.EditPlayerView, name='edit-player'),
     path('players', views.ListPlayersView, name='all-players'),
     path('view/tdf/', views.TdfView, name='all-tdf'),
