6a22893e2c47d03463797e8dbb5932c24b0516f3
Ryan Bown <rbown1993@hotmail.com>
2026-08-31 19:47:59 -0400

    added edit player js

---
+++ cityinvitecalc/static/js/load/edit_player_load.js
@@ -0,0 +1,19 @@
+function UpdatePlayer(field_name, new_value){
+
+}
+
+function SavePokemonId(){
+    tblPlayer = document.getElementById("tblPlayer")
+    txtNewPkmnID = document.getElementById("txtNewPkmnID")
+
+}
+
+function SaveFirstName(){
+    tblPlayer = document.getElementById("tblPlayer")
+    txtNewFirstName = document.getElementByIdtxtNewFirstName("")
+}
+
+function SaveLastName(){
+    tblPlayer = document.getElementById("tblPlayer")
+
+}
